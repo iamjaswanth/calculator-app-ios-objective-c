312e2b980d9d314cd61d4b55e09b8ecb04e12f55
jaswanth <jaswanthsrm@gmail.com>
2015-06-03 15:51:35 +0530

    iOS calculator app
    iOS calculator app  with objetive -c

--- calculator/ViewController.h
+++ calculator/ViewController.h
@@ -9,6 +9,22 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+{
+    int total;
+    int mode;
+    NSString *valuestring;
+    IBOutlet UILabel *label;
+    BOOL lastButtonWasMode;
+    
+    
+}
+
+-(IBAction)tappedClear:(id)sender;
+-(IBAction)tappedNumber:(UIButton*)btn;
+-(IBAction)tappedPlus:(id)sender;
+-(IBAction)tappedMinus:(id)sender;
+-(IBAction)tappedMultiply:(id)sender;
+-(IBAction)tappedEqulas:(id)sender;
 
 
 @end
--- calculator/ViewController.m
+++ calculator/ViewController.m
@@ -17,11 +17,79 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    valuestring = @"";
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+-(IBAction)tappedClear:(id)sender {
+    total = 0;
+    valuestring = @"";
+    label.text = @"0";
+    
+}
+-(IBAction)tappedNumber:(UIButton*)btn {
+    int num = btn.tag;
+    if (num == 0 && total == 0) {
+        return;
+    }
+    
+    if (lastButtonWasMode) {
+        lastButtonWasMode = NO;
+        valuestring = @"";
+    }
+    NSString *numAsString = [NSString stringWithFormat:@"%i",num];
+    valuestring = [valuestring stringByAppendingString:numAsString];
+    label.text = valuestring;
+    if (total == 0) {
+        total = [valuestring intValue];
+    }
+}
+-(IBAction)tappedPlus:(id)sender {
+    [self setMode:1];
+    
+}
+-(IBAction)tappedMinus:(id)sender {
+    [self setMode: -1];
+    
+}
+
+-(IBAction)tappedMultiply:(id)sender {
+    [self setMode: 2];
+    
+}
+-(IBAction)tappedEqulas:(id)sender {
+    
+    if (mode == 0) {
+        return ;
+    }
+    if (mode == 1) {
+        total += [valuestring intValue];
+    }
+    if (mode == -1) {
+        total -= [valuestring intValue];
+    }
+    if (mode ==  2) {
+        total *= [valuestring intValue];
+    }
+    
+    valuestring = [NSString stringWithFormat:@"%i",total];
+    label.text = valuestring;
+    mode = 0;
+    
+}
+
+-(void)setMode:(int)m {
+    
+    mode = m;
+    lastButtonWasMode = YES ;
+    total = [valuestring intValue];
+    
+    
 }
 
 @end
